c5ff830884f50fbe8a9a93be2841395719e11f0e
IsmailBENJAAFAR <benjaafarismail20@gmail.com>
2024-10-21 18:39:46 +0100

    add post endpoint

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+let items = [];
+
+app.post('/items', (req, res) => {
+    const newItem = {
+        id: items.length + 1,
+        name: req.body.name
+    };
+    items.push(newItem);
+    res.status(201).json(newItem);
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
